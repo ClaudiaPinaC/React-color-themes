1606bec27c4e8a2b727eb03da265a072ba6d5776
ClaudiaPinaC <claudiapinac524@gmail.com>
2026-02-11 02:21:33 -0800

    Adding counter and commenting theme provider code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,64 @@
-import { useState, createContext, useContext, useLayoutEffect } from 'react';
+import {
+  useState,
+  // createContext, 
+  // useContext, 
+  // useLayoutEffect 
+} from 'react';
 import './App.css';
 
 function App() {
-  const ThemeContext = createContext<{ theme: string; toggleTheme: () => void } | undefined>(undefined);
+  // const ThemeContext = createContext<{ theme: string; toggleTheme: () => void } | undefined>(undefined);
 
-  const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-    const initialTheme = () => localStorage.getItem('THEME') || 'dark-mode';
-    const [theme, setTheme] = useState(initialTheme());
-    const toggleTheme = () => 
-      setTheme((theme) => (theme === 'light-mode' ? 'dark-mode' : 'light-mode'));
+  // const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  //   const initialTheme = () => localStorage.getItem('THEME') || 'dark-mode';
+  //   const [theme, setTheme] = useState(initialTheme());
+  //   const toggleTheme = () =>
+  //     setTheme((theme) => (theme === 'light-mode' ? 'dark-mode' : 'light-mode'));
 
-    useLayoutEffect(() => {
-      localStorage.setItem('THEME', theme);
-      if(theme === 'light-mode') {
-        document.documentElement.classList.remove('dark-mode');
-        document.documentElement.classList.add('light-mode');
-      } else {
-        document.documentElement.classList.remove('light-mode');
-        document.documentElement.classList.add('dark-mode');
-      }
-    }, [theme]);
+  //   useLayoutEffect(() => {
+  //     localStorage.setItem('THEME', theme);
+  //     if (theme === 'light-mode') {
+  //       document.documentElement.classList.remove('dark-mode');
+  //       document.documentElement.classList.add('light-mode');
+  //     } else {
+  //       document.documentElement.classList.remove('light-mode');
+  //       document.documentElement.classList.add('dark-mode');
+  //     }
+  //   }, [theme]);
 
-    return (
-      <ThemeContext.Provider value={{ theme, toggleTheme}}>
-        {children}
-      </ThemeContext.Provider>
-    );
-  };
+  //   return (
+  //     <ThemeContext.Provider value={{ theme, toggleTheme }}>
+  //       {children}
+  //     </ThemeContext.Provider>
+  //   );
+  // };
 
-  const useTheme = () => {
-    const context = useContext(ThemeContext);
-    if(context === undefined) {
-      throw new Error ("useTheme must be used within a ThemeProvider");
-    }
-    return context;
-  };
+  // const useTheme = () => {
+  //   const context = useContext(ThemeContext);
+  //   if (context === undefined) {
+  //     throw new Error("useTheme must be used within a ThemeProvider");
+  //   }
+  //   return context;
+  // };
 
-  const { toggleTheme } = useTheme();
+  // const { toggleTheme } = useTheme();
+
+  const [counter, setCounter] = useState(0);
 
   return (
-    <ThemeProvider>
+    <>
       <h1>Select Your Theme</h1>
       <div className="card">
-        <button onClick={toggleTheme}>
+        {/* <button onClick={toggleTheme}>
           What's your vibe today?
+        </button> */}
+        <button onClick={() => setCounter(counter + 1)}>
+          Click Here! <br />
         </button>
+        <div>Total clicks: {counter}</div>
       </div>
-    </ThemeProvider>
-  )
+    </>
+  );
 }
 
 export default App
